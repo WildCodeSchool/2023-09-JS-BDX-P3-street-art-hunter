0707966791e8e3dc77012199151e3c362410dbaf
Leslie Abajoli <leslie.abajoli1989@gmail.com>
2023-12-13 16:51:23 +0100

    style of ranking page created

--- frontend/src/pages/Ranking.jsx
+++ frontend/src/pages/Ranking.jsx
@@ -1,3 +1,64 @@
+const usersRank = [
+  {
+    id: 1,
+    name: "Leslie",
+    points: "15 000",
+  },
+  {
+    id: 2,
+    name: "Antoine",
+    points: "11 000",
+  },
+  {
+    id: 3,
+    name: "Fréd",
+    points: "8 000",
+  },
+  {
+    id: 4,
+    name: "Claire",
+    points: "8 000",
+  },
+  {
+    id: 5,
+    name: "Marie",
+    points: "7 985",
+  },
+  {
+    id: 6,
+    name: "Lucas",
+    points: "6 000",
+  },
+  {
+    id: 7,
+    name: "Anaelle",
+    points: "5 500",
+  },
+  {
+    id: 8,
+    name: "Raph",
+    points: "4 000",
+  },
+];
 export default function Ranking() {
-  return <h1>Classement</h1>;
+  return (
+    <div>
+      <h1 className="mb-40">Classement</h1>
+      <div className="container ranking-page">
+        <div className="allow-scroll">
+          {usersRank.map((rank) => (
+            <p className="mb-30" key={rank.id}>
+              <span>
+                {" "}
+                <img src="/src/assets/coin1.png" alt="coin" /> x {""}
+                {rank.points}
+              </span>
+              {""} - {""}
+              {rank.name}
+            </p>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
