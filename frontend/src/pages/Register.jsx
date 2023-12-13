60a967706d5e49e0ce089974b7eeadedef5be699
Leslie Abajoli <leslie.abajoli1989@gmail.com>
2023-12-13 21:22:05 +0100

    state added to visualize the form data in the console

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,6 +1,26 @@
+import { Link } from "react-router-dom";
+import { useState } from "react";
 import Button from "../components/Button";
 
 export default function Register() {
+  const [formData, setFormData] = useState({
+    pseudo: "",
+    email: "",
+    postal: "",
+    city: "",
+    password: "",
+    confirmation: "",
+  });
+
+  console.info(formData);
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [name]: value,
+    }));
+  };
   return (
     <div>
       <h1 className="mb-20">Inscription</h1>
@@ -11,43 +31,86 @@ export default function Register() {
               Pseudo
             </label>
             <div className="input mb-10">
-              <input type="text" name="pseudo" id="pseudo" />
+              <input
+                type="text"
+                name="pseudo"
+                id="pseudo"
+                value={formData.pseudo}
+                onChange={handleChange}
+              />
             </div>
             <label htmlFor="email" className="mb-10">
               Adresse email
             </label>
             <div className="input mb-10">
-              <input type="text" name="email" id="email" />
+              <input
+                type="text"
+                name="email"
+                id="email"
+                value={formData.email}
+                onChange={handleChange}
+              />
             </div>
             <label htmlFor="postal" className="mb-10">
               Code Postal
             </label>
             <div className="input mb-10">
-              <input type="text" name="postal" id="postal" />
+              <input
+                type="text"
+                name="postal"
+                id="postal"
+                value={formData.postal}
+                onChange={handleChange}
+              />
             </div>
             <label htmlFor="city" className="mb-10">
               Ville
             </label>
             <div className="input mb-10">
-              <input type="text" name="city" id="city" />
+              <input
+                type="text"
+                name="city"
+                id="city"
+                value={formData.city}
+                onChange={handleChange}
+              />
             </div>
 
             <label htmlFor="password" className="mb-10">
               Mot de passe
             </label>
             <div className="input mb-10">
-              <input type="text" name="password" id="password" />
+              <input
+                type="text"
+                name="password"
+                id="password"
+                value={formData.password}
+                onChange={handleChange}
+              />
             </div>
+
             <label htmlFor="confirmation" className="mb-10">
               Confirmer le mot de passe
             </label>
             <div className="input mb-10">
-              <input type="text" name="confirmation" id="confirmation" />
+              <input
+                type="text"
+                name="confirmation"
+                id="confirmation"
+                value={formData.confirmation}
+                onChange={handleChange}
+              />
             </div>
           </form>
-          <Button className="button mb-20">Valider</Button>
-          <p className="mb-10">Déjà inscrit ?</p>
-          <p className="mb-20">Connectez-vous !</p>
+          <Button type="Valider" className="button mb-20">
+            Valider
+          </Button>
+          <Link to="/connexion">
+            <p className="mb-10">
+              Déjà inscrit ? <br />
+              Connectez-vous !
+            </p>
+          </Link>
         </div>
       </div>
     </div>
