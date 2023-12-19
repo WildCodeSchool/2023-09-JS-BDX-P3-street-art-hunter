38c1a6f0c9941d9d38887ee5db8e37fa29172a24
Leslie Abajoli <leslie.abajoli1989@gmail.com>
2023-12-19 15:34:22 +0100

    update of loginContext route following merge of loginContext into User Context

--- frontend/src/context/LoginContext.jsx
+++ frontend/src/context/LoginContext.jsx
@@ -1,42 +1,42 @@
-import { createContext, useContext, useMemo, useState } from "react";
-import { useNavigate } from "react-router-dom";
-import PropTypes from "prop-types";
+// import { createContext, useContext, useMemo, useState } from "react";
+// import { useNavigate } from "react-router-dom";
+// import PropTypes from "prop-types";
 
-const loginContext = createContext();
+// const loginContext = createContext();
 
-function LoginProvider({ children }) {
-  const [connect, setConnect] = useState(false);
-  const getUsers = () => JSON.parse(localStorage.getItem("users") ?? "[]");
-  const navigate = useNavigate();
+// function LoginProvider({ children }) {
+//   // const [connect, setConnect] = useState(false);
+//   // const getUsers = () => JSON.parse(localStorage.getItem("users") ?? "[]");
+//   // const navigate = useNavigate();
 
-  const login = (credentials) => {
-    const allUsers = getUsers();
-    const checkUser = allUsers.find(
-      (user) =>
-        user.pseudo === credentials.pseudo &&
-        user.password === credentials.password
-    );
-    if (!checkUser) {
-      alert("Identifiants incorrects !");
-    } else {
-      alert(`Content de vous revoir ${credentials.pseudo}`);
-      setConnect(checkUser);
+//   // const login = (credentials) => {
+//   //   const allUsers = getUsers();
+//   //   const checkUser = allUsers.find(
+//   //     (user) =>
+//   //       user.pseudo === credentials.pseudo &&
+//   //       user.password === credentials.password
+//   //   );
+//   //   if (!checkUser) {
+//   //     alert("Identifiants incorrects !");
+//   //   } else {
+//   //     alert(`Content de vous revoir ${credentials.pseudo}`);
+//   //     setConnect(checkUser);
 
-      return navigate("/");
-    }
-    return null;
-  };
+//   //     return navigate("/");
+//   //   }
+//   //   return null;
+//   // };
 
-  const context = useMemo(() => ({ login, connect, setConnect }), [connect]);
+//   const context = useMemo(() => ({ login, connect, setConnect }), [connect]);
 
-  return (
-    <loginContext.Provider value={context}>{children}</loginContext.Provider>
-  );
-}
+//   return (
+//     <loginContext.Provider value={context}>{children}</loginContext.Provider>
+//   );
+// }
 
-LoginProvider.propTypes = {
-  children: PropTypes.node.isRequired,
-};
+// LoginProvider.propTypes = {
+//   children: PropTypes.node.isRequired,
+// };
 
-export default LoginProvider;
-export const useLogin = () => useContext(loginContext);
+// export default LoginProvider;
+// export const useLogin = () => useContext(loginContext);
