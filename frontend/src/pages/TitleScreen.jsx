4cdcefd61f44c52c755b3c7a998092d27329eb0e
Anaelle Cauvin <anaelle.cauvin@epitech.eu>
2024-01-10 14:21:17 +0100

    [ADD] clouds and verdure

--- frontend/src/pages/TitleScreen.jsx
+++ frontend/src/pages/TitleScreen.jsx
@@ -4,10 +4,14 @@ export default function TitleScreen() {
       <div className="container intro-container h-100">
         <div className="has-two-children d-flex d-flex-column h-100">
           <div className="d-flex d-flex-center">
+            <div className="cloud cloud-1" />
+            <div className="cloud cloud-2" />
             <img className="logo mt-60" src="src/assets/logo.png" alt="logo" />
           </div>
           <div className="d-flex d-flex-center">
             <h4>Cliquez pour commencer</h4>
+            <div className="hill" />
+            <div className="bush" />
           </div>
         </div>
       </div>
