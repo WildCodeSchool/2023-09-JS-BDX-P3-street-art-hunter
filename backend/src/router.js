8569be7bce8b927d5c3addbc0ff4bc1cb7b7fa04
Anaelle Cauvin <anaelle.cauvin@epitech.eu>
2024-01-10 17:09:03 +0100

    [ADD] protection from /administration /connexion /inscription

--- backend/src/router.js
+++ backend/src/router.js
@@ -104,6 +104,14 @@ router.post("/uploads", (req, res) => {
   });
 });
 
+router.get("/admin", authMiddleware, authAdminMiddleware, (req, res) => {
+  res.status(200).json({ success: true, message: "Welcome Admin" });
+});
+
+router.get("/online", authMiddleware, (req, res) => {
+  res.status(200).json({ success: true, message: "Welcome" });
+});
+
 /* ************************************************************************* */
 
 module.exports = router;
