a40bcc784b68b2c6b964afd84943949cb72fa27d
Leslie Abajoli <leslie.abajoli1989@gmail.com>
2024-01-30 15:09:04 +0100

    added css admin page

--- frontend/src/components/ChangeStreetArt.jsx
+++ frontend/src/components/ChangeStreetArt.jsx
@@ -25,70 +25,88 @@ function ChangeStreetArt() {
   };
 
   return (
-    <div className="container-extra-small">
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="image">Image</label>
+    <div className="container-extra-small allow-scroll-container">
+      <form onSubmit={handleSubmit} className="admin-item-infos ">
+        <label className="mb-10" htmlFor="image">
+          Image
+        </label>
         <input
           type="text"
           name="image"
           id="image"
+          className="mb-10"
           value={changeArt.image}
           onChange={(e) =>
             setChangeArt({ ...changeArt, image: e.target.value })
           }
         />
-        <label htmlFor="title">Titre</label>
+        <label className="mb-10" htmlFor="title">
+          Titre
+        </label>
         <input
           type="text"
           name="title"
           id="title"
+          className="mb-10"
           value={changeArt.title || ""}
           onChange={(e) =>
             setChangeArt({ ...changeArt, title: e.target.value })
           }
         />
-        <label htmlFor="author">Auteur :</label>
+        <label className="mb-10" htmlFor="author">
+          Auteur :
+        </label>
         <input
           type="text"
           name="author"
           id="author"
+          className="mb-10"
           value={changeArt.author || ""}
           onChange={(e) =>
             setChangeArt({ ...changeArt, author: e.target.value })
           }
         />
-        <label htmlFor="address">Adresse : </label>
+        <label className="mb-10" htmlFor="address">
+          Adresse :{" "}
+        </label>
         <input
           type="text"
           name="address"
           id="address"
+          className="mb-10"
           value={changeArt.address || ""}
           onChange={(e) =>
             setChangeArt({ ...changeArt, address: e.target.value })
           }
         />
 
-        <label htmlFor="longitude">longitude: </label>
+        <label className="mb-10" htmlFor="longitude">
+          Longitude:{" "}
+        </label>
         <input
           type="text"
           name="longitude"
           id="longitude"
+          className="mb-10"
           value={changeArt.longitude || ""}
           onChange={(e) =>
             setChangeArt({ ...changeArt, longitude: e.target.value })
           }
         />
-        <label htmlFor="latitude">latitude: </label>
+        <label className="mb-10" htmlFor="latitude">
+          Latitude:{" "}
+        </label>
         <input
           type="text"
           name="latitude"
           id="latitude"
+          className="mb-10"
           value={changeArt.latitude || ""}
           onChange={(e) =>
             setChangeArt({ ...changeArt, latitude: e.target.value })
           }
         />
-        <Button type="submit" className="mt-20 button mb-20">
+        <Button type="submit" className=" button mb-20">
           Modifier
         </Button>
       </form>
--- frontend/src/components/DisplayStreetArt.jsx
+++ frontend/src/components/DisplayStreetArt.jsx
@@ -5,7 +5,7 @@ function DisplayStreetArt() {
   const loaderData = useLoaderData();
 
   return (
-    <div className="container-extra-small">
+    <div className="container-extra-small allow-scroll-container admin-streetarts">
       <div className="admin-item-list">
         <div key={loaderData?.streetArt?.id} className="admin-item">
           <div className="admin-item-infos">
@@ -16,12 +16,16 @@ function DisplayStreetArt() {
             <p>
               {loaderData?.streetArt?.title}
               <br />
-              Par {loaderData?.streetArt?.author}
+              Par : {loaderData?.streetArt?.author}
             </p>
-            <p className="mb-10">Adresse: {loaderData?.streetArt?.address}</p>
+            <p className="mb-10">Adresse : {loaderData?.streetArt?.address}</p>
 
-            <p className="mb-10">Lng: {loaderData?.streetArt?.longitude}</p>
-            <p className="mb-10">Lat: {loaderData?.streetArt?.latitude}</p>
+            <p className="mb-10">
+              Longitude : {loaderData?.streetArt?.longitude}
+            </p>
+            <p className="mb-10">
+              Latitude : {loaderData?.streetArt?.latitude}
+            </p>
           </div>
         </div>
       </div>
